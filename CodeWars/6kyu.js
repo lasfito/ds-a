468ac39524229a6c9f98c5a5789ebb353495eb47
lasfito <hola@lasfi.to>
2021-09-08 17:53:07 -0500

    katas

--- CodeWars/6kyu.js
+++ CodeWars/6kyu.js
@@ -1,4 +1,4 @@
-// Enough is enough!
+// k01 - Enough is enough!
 // Alice and Bob were on a holiday. Both of them took many pictures of the places they've been, and now they want to show Charlie their entire collection. However, Charlie doesn't like these sessions, since the motive usually repeats. He isn't fond of seeing the Eiffel tower 40 times. He tells them that he will only sit during the session if they show the same motive at most N times. Luckily, Alice and Bob are able to encode the motive as a number. Can you help them to remove numbers such that their list contains each number only up to N times, without changing the order?
 
 // Task
@@ -32,4 +32,59 @@ function deleteNth(arr,n){
     });
   }
 
- // https://www.codewars.com/kata/554ca54ffa7d91b236000023/solutions/javascript
+  function deleteNth(arr,x){
+    var count = {};
+    return arr.filter(function(a){
+      count[a] = ~~count[a]+1;
+      return count[a]<=x;
+    })
+  } //BITWISE NOT OPERATOR - So when you call ~undefined you get -1, and he wants 0, so its ~~. 
+
+
+  // // k02 - Tribonacci Sequence
+  // So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting input (AKA signature), we have this sequence:
+
+  // [1, 1 ,1, 3, 5, 9, 17, 31, ...]
+  // But what if we started with [0, 0, 1] as a signature? As starting with [0, 1] instead of [1, 1] basically shifts the common Fibonacci sequence by once place, you may be tempted to think that we would get the same sequence shifted by 2 places, but that is not the case and we would get:
+  
+  // [0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
+  // Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
+  
+  // Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array.
+
+  function tribonacci(signature,n){
+    // condicion inicial
+    if (n == 0) return [];
+    if (n == 1) return [signature[0]];
+    if (n == 2) return signature[0] + signature[1];
+    if (n == 3) return signature.reduce((accumulator, curr) => accumulator + curr);
+    
+    //variables
+    let contador = 0;
+    const newArr = [...signature];
+    
+    while ( contador < n -3) {
+   
+    //sumar 3 originales
+    let tripleSuma = signature.reduce((accumulator, curr) => accumulator + curr);
+    //actualizar nuevo array
+    newArr.push(tripleSuma)
+    // actualizar base 3
+    signature.push(tripleSuma);
+    signature.shift()
+    // actualizar contador while  
+    contador += 1;  
+    
+      
+    
+    }
+   return newArr;
+  }
+
+  function tribonacci(s,n){
+    var arr = [];
+    for(var i=0; i<n; i++) {
+      arr.push((i<3) ? s[i] : arr[i-1]+arr[i-2]+arr[i-3]);
+    }
+    return arr;
+  }
--- CodeWars/7kyu.js
+++ CodeWars/7kyu.js
@@ -1,6 +1,4 @@
-"use strict"
-
-//Find the calculation type
+// k01 - Find the calculation type
 // You have to create a function calcType, which receives 3 arguments: 2 numbers, and the result of an unknown operation performed on them (also a number).
 
 // Based on those 3 values you have to return a string, that describes which operation was used to get the given result.
@@ -25,4 +23,57 @@ function calcType(a, b, res) {
   a / b === res ? 'division' : 
   null;
 
+  // k02 - Friend or foe
+//   Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// i.e.
+
+// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+// Note: keep the original order of the names in the output.
+
+function friend(friends){
+  const amigos = [];
+    friends.forEach((persona)=>{
+      
+      (persona.length == 4 && amigos.push(persona))
+   })
+      
+    return amigos;
+    
+  }
+
+
+  function friend(friends){
+    return friends.filter(n => n.length === 4)
+  }
+
+
+  // k03 - accum no time for story xd
+//   This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+let funk = (s)=> {
+ 
+  let arr = [];
+  
+  [...s].forEach ((letra, index)=>{
+    for (let i = -1; i< index; i++) {
+      (i == -1) ? arr.push(letra.toUpperCase()) : arr.push(letra);
+    }
+    
+  })
+  
+  
+   
+   return arr.toString();
+ }
   
